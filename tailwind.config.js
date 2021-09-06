75508d7a97c132c55be43801491703277cb02531
Hanif <ilhamhanif1512@gmail.com>
2021-09-06 14:03:43 +0700

    Uhuy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
